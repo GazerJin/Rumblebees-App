243baae531f420edf3cbfa5922df9129e4849297
Daniella Limbag <92845499+daniellalimbag@users.noreply.github.com>
2024-11-22 20:24:09 +0800

    Refactor: Updated navbar in main activity and refactored FirstFragment class to CardFragment

--- app/src/main/java/com/mobdeve/s15/bautista/samantha/gamedesapp/MainActivity.java
+++ app/src/main/java/com/mobdeve/s15/bautista/samantha/gamedesapp/MainActivity.java
@@ -1,77 +1,51 @@
 package com.mobdeve.s15.bautista.samantha.gamedesapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.mobdeve.s15.bautista.samantha.gamedesapp.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // Initialize navbar
+        BottomNavigationView navbar = findViewById(R.id.navbarBnv);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.navbar, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        // Set default fragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new CardsFragment()).commit();
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        // Set up item selection listener
+        navbar.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+/*
+            // Navigate to selected fragment
+            switch (item.getItemId()) {
+                case R.id.fragment_cards:
+                    selectedFragment = new CardsFragment();
+                    break;
+                case R.id.fragment_board:
+                    selectedFragment = new BoardFragment();
+                    break;
+                case R.id.fragment_combat:
+                    selectedFragment = new CombatFragment();
+                    break;
+                case R.id.fragment_help:
+                    selectedFragment = new HelpFragment();
+                    break;
+            }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+            */
+            return true;
+        });
+
     }
 }
